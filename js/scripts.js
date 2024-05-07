1d1356407ad204509e38d858e0e95791adecbfcb
Welington Silva <welingtonsilva401@gmail.com>
2024-05-07 20:25:39 -0300

    atualização dos estilos

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,38 @@ icons.map((devIcon) => {
   });
 });
 
-const app = document.getElementById("title-home");
+// const app = document.getElementById("title-home");
 
-var typewriter = new Typewriter(app, {
+// var typewriter = new Typewriter(app, {
+//   loop: true,
+// });
+
+// typewriter
+
+//   .typeString("Judges a book by its cover...")
+
+//   .pauseFor(2500)
+
+//   .pauseFor(200)
+//   .deleteChars(25)
+//   .start();
+
+// setTimeout(() => {}, 3000);
+
+
+const homeH2 = document.getElementById("home-h2");
+
+var typewriter = new Typewriter(homeH2, {
   loop: true,
 });
 
 typewriter
 
-  .typeString("Judges a book by its cover...")
+  .typeString("I'm a Software Engineer.")
 
   .pauseFor(2500)
 
   .pauseFor(200)
-  .deleteChars(25)
+  .deleteChars(20)
   .start();
+
