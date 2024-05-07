03b90ea6f029c73fa6ad98e69012cdb084b0f8f4
Welington Silva <welingtonsilva401@gmail.com>
2024-05-07 01:41:05 -0300

    refatoração do css

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,6 @@
 AOS.init();
 
-
 const icons = Array.from(document.querySelectorAll(".icon"));
-
-// const sections = Array.from(document.getElementsByTagName("section"));
-
-// const observer = new IntersectionObserver((entries) => {
-//   Array.from(entries).map((entry) => {
-
-//     const entriesTarget = entry.target;
-
-//     if (entry.isIntersecting) {
-//       entriesTarget.classList.add("show");
-
-//       entriesTarget.classList.remove("hidden-section");
-//     } else {
-//       entriesTarget.classList.remove("show");
-
-//       entriesTarget.classList.add("hidden-section");
-//     }
-//   });
-// });
-
-// sections.map((sectionDom) => {
-//   observer.observe(sectionDom);
-// });
 
 icons.map((devIcon) => {
   devIcon.addEventListener("click", () => {
@@ -33,17 +9,18 @@ icons.map((devIcon) => {
   });
 });
 
-const app = document.getElementById('title-home');
+const app = document.getElementById("title-home");
 
 var typewriter = new Typewriter(app, {
-    loop: true
+  loop: true,
 });
 
-typewriter.typeString('Judges a book by its cover...')
-.pauseFor(2000)
-// .deleteAll()
-.pauseFor(200)
-.deleteChars(25)
-.start();
+typewriter
 
+  .typeString("Judges a book by its cover...")
 
+  .pauseFor(2500)
+
+  .pauseFor(200)
+  .deleteChars(25)
+  .start();
