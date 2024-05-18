c55e5d5255325191b6e384eb891d5ff4cfd0c5fb
Welington Silva <welingtonsilva401@gmail.com>
2024-05-18 18:45:33 -0300

    criação menu hamburguer

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 AOS.init();
 
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+const navLinks = Array.from(document.querySelectorAll(".nav-link"));
+
 const icons = Array.from(document.querySelectorAll(".icon"));
 const homeH2 = document.getElementById("home-h2");
 const toTopBtn = document.querySelector("#to-top-btn");
+
+hamburguer.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+navLinks.map((linkNavgation) => {
+  linkNavgation.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+});
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
